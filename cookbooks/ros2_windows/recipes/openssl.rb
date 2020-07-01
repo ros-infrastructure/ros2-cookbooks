a14df46c883f826a3e09cb1bcd44798abd323269
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-07-01 13:21:20 -0400

    Use different OpenSSL version for different ROS distros.
    OpenSSL 1.1.1g also changed the default installation path.

--- cookbooks/ros2_windows/recipes/openssl.rb
+++ cookbooks/ros2_windows/recipes/openssl.rb
@@ -1,17 +1,32 @@
+openssl_versions = {
+  "dashing" => "1_0_2u",
+  "eloquent" => "1_0_2u",
+  "foxy" => "1_1_1g",
+  "rolling" => "1_1_1g",
+}.freeze
+
+openssl_version = openssl_versions[node["ros2_windows"]["ros_distro"]]
+openssl_conf_dir = if openssl_version == "1_1_1g"
+                     'C:\Program Files\\OpenSSL-Win64'
+                   else
+                     'C:\OpenSSL-Win64'
+                   end
+
+
 windows_package 'openssl' do
-  source 'https://slproweb.com/download/Win64OpenSSL-1_0_2u.exe'
+  source "https://slproweb.com/download/Win64OpenSSL-#{openssl_version}.exe"
   options '/VERYSILENT'
 end
 
 windows_env 'OPENSSL_CONF' do
   key_name 'OPENSSL_CONF'
-  value 'C:\\OpenSSL-Win64\\bin\\openssl.cfg'
+  value "#{openssl_conf_dir}\\bin\\openssl.cfg"
   action :create
 end
 
 windows_env 'PATH' do
   key_name 'PATH'
-  value 'C:\\OpenSSL-Win64\\bin'
+  value "#{openssl_conf_dir}\\bin"
   delim ';'
   action :modify
 end
