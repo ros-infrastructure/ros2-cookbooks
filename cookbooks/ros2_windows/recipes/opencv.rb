a3c027f68119e4cf018e8390bd34f01aed0f6276
Stephen Brawner <brawner@gmail.com>
2020-08-04 15:03:36 -0700

    Correct installer location and opencv path
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- cookbooks/ros2_windows/recipes/opencv.rb
+++ cookbooks/ros2_windows/recipes/opencv.rb
@@ -1,5 +1,5 @@
 seven_zip_archive 'open_cv_zip' do
-  path 'C:\\opencv'
+  path 'C:\\'
   source 'https://github.com/ros2/ros2/releases/download/opencv-archives/opencv-3.4.6-vc16.VS2019.zip'
   overwrite true
 end
@@ -12,7 +12,7 @@ end
 
 windows_env 'PATH' do
   key_name 'PATH'
-  value 'C:\\opencv'
+  value 'C:\\opencv\\x64\\vc16\\bin'
   delim ';'
   action :modify
 end
