df47167c425388628d48172d8c0858e5a4db4876
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-01-19 11:08:42 -0800

    Don't run qt maintenance tool.
    The maintenance tool has proved ironically high maintenance and brittle
    and frequently causes platform failures when building.
    
    To get unblocked let's try not running it and eventually we may move to
    using the offline Qt installer to cut down on brittleness.

--- cookbooks/ros2_windows/recipes/qt5.rb
+++ cookbooks/ros2_windows/recipes/qt5.rb
@@ -18,42 +18,42 @@ template  'qt-installer.qs' do
   sensitive true
 end
 
-template  'qt-maintenance.qs' do
-  source 'qt-maintenance.qs.erb'
-  variables Hash[
-    qt_account_email: node['ros2']['qt_account_email'],
-    qt_account_password: node['ros2']['qt_account_password'],
-  ]
-  sensitive true
-end
+#template  'qt-maintenance.qs' do
+#  source 'qt-maintenance.qs.erb'
+#  variables Hash[
+#    qt_account_email: node['ros2']['qt_account_email'],
+#    qt_account_password: node['ros2']['qt_account_password'],
+#  ]
+#  sensitive true
+#end
 
 # Install Qt5 with automated install script, no msvc2019 version exists but 2017 is compatible
 # Updater/silentUpdate options did not work for me.
 # Install scripts finds and installs the most recent LTS version
 error_filename = File.join(Dir.home(), "qt_install.err")
 
-# Update maintenance tool first, if necessary
-windows_package 'Qt Maintenance Update' do
-  source 'c:\\Qt\\MaintenanceTool.exe'
-  installer_type :custom
-  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
-  # Instead error information is written to the ErrorLogname below
-  returns [0]
-  options 'up qt.tools.maintenance'
-  timeout 600
-  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
-end
-
-windows_package 'Qt Maintenance' do
-  source 'c:\\Qt\\MaintenanceTool.exe'
-  installer_type :custom
-  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
-  # Instead error information is written to the ErrorLogname below
-  returns [0, 1, 3]
-  options '--script qt-maintenance.qs MsvcVersion=2019 ErrorLogname="' + error_filename + '"'
-  timeout 2000
-  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
-end
+## Update maintenance tool first, if necessary
+#windows_package 'Qt Maintenance Update' do
+#  source 'c:\\Qt\\MaintenanceTool.exe'
+#  installer_type :custom
+#  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
+#  # Instead error information is written to the ErrorLogname below
+#  returns [0]
+#  options 'up qt.tools.maintenance'
+#  timeout 600
+#  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
+#end
+#
+#windows_package 'Qt Maintenance' do
+#  source 'c:\\Qt\\MaintenanceTool.exe'
+#  installer_type :custom
+#  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
+#  # Instead error information is written to the ErrorLogname below
+#  returns [0, 1, 3]
+#  options '--script qt-maintenance.qs MsvcVersion=2019 ErrorLogname="' + error_filename + '"'
+#  timeout 2000
+#  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
+#end
 
 windows_package 'Qt Install' do
   source 'http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe'
