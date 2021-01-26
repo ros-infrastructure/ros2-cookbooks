aa9dadc90c3432871109c9f2d6968d8753feb7d5
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-01-25 16:59:22 -0800

    Use offline installer for Qt 5.12 when called for.

---
+++ cookbooks/ros2_windows/attributes/qt5.rb
@@ -0,0 +1,1 @@
+default['ros2_windows']['qt5']['qt_mirror_url'] = 'https://download.qt.io'
--- cookbooks/ros2_windows/recipes/qt5_offline.rb
+++ cookbooks/ros2_windows/recipes/qt5_offline.rb
@@ -18,52 +18,19 @@ template  'qt-installer.qs' do
   sensitive true
 end
 
-template  'qt-maintenance.qs' do
-  source 'qt-maintenance.qs.erb'
-  variables Hash[
-    qt_account_email: node['ros2']['qt_account_email'],
-    qt_account_password: node['ros2']['qt_account_password'],
-  ]
-  sensitive true
-end
-
-# Install Qt5 with automated install script, no msvc2019 version exists but 2017 is compatible
-# Updater/silentUpdate options did not work for me.
-# Install scripts finds and installs the most recent LTS version
+qt_mirror_url = node['ros2_windows']['qt5']['qt_mirror_url']
 error_filename = File.join(Dir.home(), "qt_install.err")
-
-# Update maintenance tool first, if necessary
-windows_package 'Qt Maintenance Update' do
-  source 'c:\\Qt\\MaintenanceTool.exe'
-  installer_type :custom
-  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
-  # Instead error information is written to the ErrorLogname below
-  returns [0]
-  options 'up qt.tools.maintenance'
-  timeout 600
-  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
-end
-
-windows_package 'Qt Maintenance' do
-  source 'c:\\Qt\\MaintenanceTool.exe'
-  installer_type :custom
-  # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
-  # Instead error information is written to the ErrorLogname below
-  returns [0, 1, 3]
-  options '--script qt-maintenance.qs MsvcVersion=2019 ErrorLogname="' + error_filename + '"'
-  timeout 2000
-  only_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
-end
-
 windows_package 'Qt Install' do
-  source 'http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe'
+  # Qt 5.12 was the last version of Qt to be provided an offline installer for open source usage.
+  source "#{qt_mirror_url}/official_releases/qt/5.12/5.12.10/qt-opensource-windows-x86-5.12.10.exe"
+  checksum "e315ec89000ae08d51c248f251ce670036b565e1d3a07df50e32557b8fd8246e"
   installer_type :custom
   # I couldn't find documentation, but return codes don't seem to correspond with an actual failure.
   # Instead error information is written to the ErrorLogname below
   returns [0, 1, 3]
-  options '--script qt-installer.qs MsvcVersion=2019 ErrorLogname="' + error_filename + '"'
+  options %{--st "#{qt_mirror_url}" --script qt-installer.qs --verbose MsvcVersion=2019 ErrorLogname="#{error_filename}"}
   timeout 2000
-  not_if {::File.exist?('c:\\Qt\\MaintenanceTool.exe')}
+  not_if {::File.exist?('c:\\Qt\\Qt5.12.10\\MaintenanceTool.exe')}
 end
 
 ruby_block 'error_exist?' do
