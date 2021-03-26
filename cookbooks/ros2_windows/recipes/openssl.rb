456e5c70184199f8e83ca1a847e0c14ed62f2a56
Emerson Knapp <eknapp@amazon.com>
2021-03-26 10:41:11 -0700

    Update to openssl1_1_1k after j version was removed - fix 404 errors in windows CI
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- cookbooks/ros2_windows/recipes/openssl.rb
+++ cookbooks/ros2_windows/recipes/openssl.rb
@@ -1,8 +1,8 @@
 openssl_versions = {
   "dashing" => "1_0_2u",
   "eloquent" => "1_0_2u",
-  "foxy" => "1_1_1j",
-  "rolling" => "1_1_1j",
+  "foxy" => "1_1_1k",
+  "rolling" => "1_1_1k",
 }.freeze
 
 openssl_version = openssl_versions[node["ros2_windows"]["ros_distro"]]
