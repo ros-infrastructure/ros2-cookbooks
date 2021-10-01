18b4eb1773a0ec3cef977f046323e214a145ff45
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-10-01 13:12:44 -0700

    Guard against a url parameter with no trailing slash.
    This will result in a doubled slash in the url in some cases but that
    should be benign to most web servers.

--- cookbooks/ros2_windows/recipes/xmllint.rb
+++ cookbooks/ros2_windows/recipes/xmllint.rb
@@ -1,19 +1,19 @@
 xmllint_url = node['ros2_windows']['xmllint']['xmllint_download_url']
 seven_zip_archive 'libxml2' do
   path 'C:\\xmllint'
-  source xmllint_url + 'libxml2-2.9.3-win32-x86_64.7z'
+  source xmllint_url + '/libxml2-2.9.3-win32-x86_64.7z'
   overwrite true
 end
 
 seven_zip_archive 'zlib' do
   path 'C:\\xmllint'
-  source xmllint_url + 'zlib-1.2.8-win32-x86_64.7z'
+  source xmllint_url + '/zlib-1.2.8-win32-x86_64.7z'
   overwrite true
 end
 
 seven_zip_archive 'iconv' do
   path 'C:\\xmllint'
-  source xmllint_url + 'iconv-1.14-win32-x86_64.7z'
+  source xmllint_url + '/iconv-1.14-win32-x86_64.7z'
   overwrite true
 end
 
