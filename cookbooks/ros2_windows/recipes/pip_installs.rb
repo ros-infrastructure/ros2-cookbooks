49ae1d61f416fbd3b557d402394ea6eace02a873
Chris Lalancette <clalancette@openrobotics.org>
2022-01-24 17:01:15 -0500

    Switch from using ifcfg to using psutil. (#39)
    * Switch from using ifcfg to using psutil.
    
    This is to correspond to a change in the ros2doctor code which
    does the same.  For older distributions (i.e. Foxy and Galactic),
    continue to install ifcfg.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

--- cookbooks/ros2_windows/recipes/pip_installs.rb
+++ cookbooks/ros2_windows/recipes/pip_installs.rb
@@ -6,7 +6,6 @@ required_pip_packages = %w[
   catkin_pkg
   cryptography
   EmPy
-  ifcfg
   lark-parser
   lxml
   netifaces
@@ -19,6 +18,15 @@ required_pip_packages = %w[
   coverage
   mock
 ]
+
+ros2doctor_network_dependency = {
+  "foxy" => "ifcfg",
+  "galactic" => "ifcfg",
+  "rolling" => "psutil",
+}.freeze
+
+
+required_pip_packages << ros2doctor_network_dependency[node["ros2_windows"]["ros_distro"]]
 
 development_pip_packages = %w[
   flake8
